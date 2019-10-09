f949e0842744eb5cb7f1f52f2e711680ee42961a
Aravindh Swaminathan Shankar <shankar@koekkoek.lorentz.leidenuniv.nl>
2019-10-09 15:02:22 +0200

    added the displayStuff and included in main

--- Hello-World.cpp
+++ Hello-World.cpp
@@ -1,7 +1,14 @@
 #include<iostream> 
+#include<string>
+
+void displayStuff(std::string);
 
 int main()
 {
 	std::cout<< "Hello World!" << std::endl;
+	displayStuff("Input");
 	return (0);
 }
+
+
+
---
+++ displayStuff.cpp
@@ -0,0 +1,10 @@
+#include<vector>
+#include<string>
+#include<iostream>
+
+// This is the function file for the displayStuff method
+
+void displayStuff(std::string IP)
+{
+	std::cout << "The given string argument is " << IP << std::endl;
+}
